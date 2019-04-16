88a728bb530210afba898fc0af55233313b57c0e
fukaiWang <fukwang@microsoft.com>
2019-04-16 17:49:26 +0800

    clip picture

--- src/index.js
+++ src/index.js
@@ -1,133 +1,22 @@
 import '@/assets/styles/index.scss';
 import '@/utils/domUtils';
 import { img2base64, video2base64 } from '@/utils/canvas';
-import Clipper from '@/utils/clipper';
 import $ from 'jquery';
+import DEFAULT_OPTION from '@/utils/defaultOption';
 var jsviews = require('jsviews')($);
-var jsrender = require('jsrender')($);
-
-const DEFAULT_OPTION = {
-  container: $('#feedback'),
-  trigger: $('#btn'),
-  submitCallback: () => {},
-  theme: '#3986FF',
-  license: '',
-  title: 'Send feedback',
-  placeholder: 'Describe your issue or share your ideas',
-  requiredTip: 'description is required',
-  editTip: 'Click to highlight or hide info',
-  loadingTip: 'loading screenshot...',
-  checkboxLabel: 'Include screenshot',
-  cancelLabel: 'cancel',
-  confirmLabel: 'send',
-  hightlightTip: 'Highlight issues',
-  hideTip: 'Hide sensitive info',
-  editDoneLabel: 'Done',
-  state: {
-    isEditMode: false,
-    toolBarType: 'highlight'
-  }
-};
-var app;
 
 const UserTrack = {
   _options: {},
   init(option) {
-    app = Object.assign({}, DEFAULT_OPTION, option);
     this._options = Object.assign({}, DEFAULT_OPTION, option);
-    this._renderFeedbackModal();
-    this._bindEvent();
     this._parseTags();
-    console.log(app);
+    this._renderFeedbackModal();
   },
   _renderFeedbackModal() {
-    var jsRenderTpl;
-
-    jsRenderTpl = jsrender.templates(require('@/templates/feedback.html'));
-    jsRenderTpl.link('#feedback', app);
-  },
-  _bindEvent() {
-    // bind trigger event
-
-    const {trigger: feedbackBtn, submitCallback } = this._options;
-
-    $(feedbackBtn).click(() => {
-      this._showFeedback();
-    });
-
-    $('.to-edit').click(() => {
-      console.log(app);
-      jsviews.observable(app).setProperty('state.isEditMode', true);
-    });
-
-    $('#feedback .checkbox .checkbox-icon').click((e) => {
-      const $target = $(e.currentTarget);
-      if ($target.hasClass('active')) {
-        $target.removeClass('active');
-        $target.siblings('svg').addClass('active');
-      } else {
-        $target.addClass('active');
-        $target.siblings('svg').removeClass('active');
-      }
-      if ($('#feedback .checkbox .checkbox-icon.include-shot').hasClass('active')) {
-        this._clip();
-        $('#feedbackDialog').find('.screenshot-area').show();
-      } else {
-        $('#feedbackDialog').find('.screenshot-area').hide();
-      }
-    });
-
-    // cancel event
-    $('#feedback .flatbutton.cancel').click(() => { this._hideFeedback();});
-    // send event
-    $('#feedback .flatbutton.confirm').click(() => {
-      // if (this.state.loading) {
-      //   this.snackbar(this.props.loadingTip || '正在加载屏幕截图...');
-      //   return;
-      // }
-      // let text = this.state.text;
-
-      // if (!text) {
-      //   this.setState({
-      //     textError: this.props.requiredTip || '必须添加说明'
-      //   });
-      //   this.refs.textarea.focus();
-      //   return;
-      // }
-      if (typeof submitCallback === 'function') {
-        const data = {
-          text: $('#feedback textarea').val(),
-          shot: $('#feedback #screenshotPrev').attr('src') || ''
-        };
-
-        if ($('#feedback .checkbox .checkbox-icon.exclude-shot').hasClass('active')) {
-          delete data.shot;
-        }
-        submitCallback(data);
-        this._hideFeedback();
-      }
-    });
-    $('#feedback .tool-bar clearfix .flatbutton done').click(() => {
-      this._hideFeedback();
-    });
-  },
-  _hideFeedback() {
-    $('#feedback').hide();
-    $('#feedbackDialog').hide();
-  },
-  _showFeedback() {
-    this._clip();
-    $('#feedback').show();
-    $('#feedbackDialog').find('.loading').show();
-  },
-  _clip() {
-    $('#screenshotPrev').attr('src', '');
-    Clipper.clip(this._options).then(dataUrl => {
-      this.imgurl = dataUrl;
-      $('#feedbackDialog').show();
-      $('#screenshotPrev').attr('src', dataUrl);
-      $('#feedbackDialog').find('.loading').hide();
-    });
+    var RenderTemplate = jsviews.templates(require('@/templates/feedback.html'));
+    $('body').append('<div id="feedback"></div>');
+    RenderTemplate.link('#feedback', this._options);
+    this._renderfinish();
   },
   _parseTags() {
     this._parseImg(document.body);
@@ -161,21 +50,8 @@ const UserTrack = {
       video.style.backgroundImage = `url(${video2base64(video)})`;
     });
   },
-  handleMouseMove(e) {
-    if (!this.move) return;
-    let toolBar = this.refs.toolBar;
-    let eX = this.eX;
-    let eY = this.eY;
-    let newEX = e.clientX + window.scrollX;
-    let newEY = e.clientY + window.scrollY;
-    let oX = newEX - eX;
-    let oY = newEY - eY;
-    let curL = parseFloat(toolBar.style.left);
-    let curT = parseFloat(toolBar.style.top);
-    toolBar.style.left = `${curL + oX}px`;
-    toolBar.style.top = `${curT + oY}px`;
-    this.eX = newEX;
-    this.eY = newEY;
+  _renderfinish() {
+    this._options.componentDidMount();
   }
 };
 
---
+++ src/utils/defaultOption.js
@@ -0,0 +1,414 @@
+
+import Clipper from '@/utils/clipper';
+import $ from 'jquery';
+const hightLightEl = ['button', 'a', 'span', 'em', 'i', 'h1', 'h2', 'h3', 'h4',
+  'h5', 'h6', 'p', 'strong', 'small', 'sub',
+  'sup', 'b', 'time', 'img', 'video', 'input', 'label', 'select', 'textarea',
+  'article', 'summary', 'section'];
+const DEFAULT_OPTION = {
+  container: $('#feedback'),
+  trigger: $('#btn'),
+  submitCallback: () => {},
+  theme: '#3986FF',
+  license: '',
+  title: 'Send feedback',
+  placeholder: 'Describe your issue or share your ideas',
+  requiredTip: 'description is required',
+  editTip: 'Click to highlight or hide info',
+  loadingTip: 'loading screenshot...',
+  checkboxLabel: 'Include screenshot',
+  cancelLabel: 'cancel',
+  confirmLabel: 'send',
+  highlightTip: 'Highlight issues',
+  hideTip: 'Hide sensitive info',
+  editDoneLabel: 'Done',
+  screenShotSrc: '',
+  state: {
+    docWidth: 0,
+    docHeight: 0,
+    winHeight: 0,
+    isEditMode: false,
+    device: 'pc',
+    text: '',
+    textError: '',
+    shotOpen: true,
+    loading: false,
+    screenshotEdit: false,
+    editMode: false,
+    toolBarType: 'highlight',
+    highlightItem: [],
+    blackItem: [],
+    snackbar: false,
+    snackbarMsg: ''
+  },
+  move: false,
+  eX: 0,
+  eY: 0,
+  ctx: null,
+  dragRect: false,
+  startX: 0,
+  startY: 0,
+  handleMoveMouseDown(e) {
+    this.move = true;
+    this.eX = e.clientX + window.scrollX;
+    this.eY = e.clientY + window.scrollY;
+  },
+  handleMoveMouseUp(e) {
+    this.move = false;
+    this.canvasMD = false;
+    if (this.dragRect) {
+      let clientX = e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft),
+        clientY = e.clientY + (document.documentElement.scrollTop || document.body.scrollTop),
+        width = this.startX - clientX,
+        height = this.startY - clientY;
+      if (Math.abs(width) < 6 || Math.abs(height) < 6) {
+        return;
+      }
+      let toolBarType = this.state.toolBarType,
+        obj = {
+          sx: clientX,
+          sy: clientY,
+          width: width,
+          height: height
+        };
+      if (width < 0) {
+        obj.sx = obj.sx + width;
+        obj.width = Math.abs(width);
+      }
+      if (height < 0) {
+        obj.sy = obj.sy + height;
+        obj.height = Math.abs(height);
+      }
+      if (toolBarType === 'highlight') {
+        $.observable(this.state.highlightItem).insert(obj);
+      } else if (toolBarType === 'black') {
+        $.observable(this.state.blackItem).insert(obj);
+        // $.observable(this).setProperty('state.blackItem', blackItem);
+      }
+      setTimeout(() => {
+        this.dragRect = false;
+        this.drawHightlightBorder();
+        this.drawHightlightArea();
+      });
+    }
+  },
+
+  handleMouseMove(e) {
+    if (!this.move) return;
+    let toolBar = $('#feedback .tool-bar.clearfix')[0];
+    let eX = this.eX;
+    let eY = this.eY;
+    let newEX = e.clientX + window.scrollX;
+    let newEY = e.clientY + window.scrollY;
+    let oX = newEX - eX;
+    let oY = newEY - eY;
+    let curL = parseFloat(toolBar.style.left);
+    let curT = parseFloat(toolBar.style.top);
+    toolBar.style.left = `${curL + oX}px`;
+    toolBar.style.top = `${curT + oY}px`;
+    this.eX = newEX;
+    this.eY = newEY;
+  },
+  canvasMouseDown(e) {
+    this.canvasMD = true;
+    this.startX = e.clientX + (document.documentElement.scrollLeft + document.body.scrollLeft);
+    this.startY = e.clientY + (document.documentElement.scrollTop + document.body.scrollTop);
+  },
+  drawHightlightBorder() {
+    let highlightItem = this.state.highlightItem;
+    highlightItem.map((data, k) => {
+      this.ctx.lineWidth = '5';
+      this.ctx.strokeStyle = '#FEEA4E';
+      this.ctx.rect(data.sx, data.sy, data.width, data.height);
+      this.ctx.stroke();
+    });
+  },
+
+  drawHightlightArea() {
+    let highlightItem = this.state.highlightItem;
+    highlightItem.map((data, k) => {
+      this.sctx.clearRect(data.sx, data.sy, data.width, data.height);
+      this.ctx.clearRect(data.sx, data.sy, data.width, data.height);
+    });
+  },
+  addEventListener() {
+    document.addEventListener('mousemove', this._documentMouseMove(), false);
+    document.addEventListener('click', this._elementHelperClick(), false);
+    window.addEventListener('resize', this._windowResize(), false);
+  },
+  windowResize() {
+    this.calcHeight();
+  },
+  calcHeight() {
+    let docWidth = document.body.clientWidth,
+      docHeight = document.body.clientHeight;
+    let windowHeight = window.innerHeight;
+    if (docHeight < windowHeight) {
+      docHeight = windowHeight;
+    }
+    $.observable(this).setProperty('state.docWidth', docWidth);
+    $.observable(this).setProperty('state.docHeight', docHeight);
+    $.observable(this).setProperty('state.winHeight', windowHeight);
+    setTimeout(() => {
+      this.initCanvas(true);
+    });
+  },
+
+  removeEventListener() {
+    document.removeEventListener('mousemove', this._documentMouseMove(), false);
+    document.removeEventListener('click', this._elementHelperClick(), false);
+    window.removeEventListener('resize', this._windowResize(), false);
+  },
+  initCanvas(init) {
+    let canvas = $('#feedback #feedbackCanvas')[0];
+    let shadowCanvas = $('#feedback #shadowCanvas')[0];
+    let docWidth = this.state.docWidth,
+      docHeight = this.state.docHeight;
+    if (!this.ctx) {
+      this.ctx = canvas.getContext('2d');
+    }
+    if (!this.sctx) {
+      this.sctx = shadowCanvas.getContext('2d');
+    }
+    if (init) {
+      canvas.style.width = docWidth;
+      canvas.style.height = docHeight;
+      shadowCanvas.style.width = docWidth;
+      shadowCanvas.style.height = docHeight;
+    }
+    canvas.width = docWidth;
+    canvas.height = docHeight;
+    shadowCanvas.width = docWidth;
+    shadowCanvas.height = docHeight;
+    this.sctx.fillStyle = 'rgba(0,0,0,0.38)';
+    this.sctx.fillRect(0, 0, docWidth, docHeight);
+  },
+  drawElementHelper(info) {
+    this.initCanvas();
+    let toolBarType = this.state.toolBarType;
+    if (toolBarType === 'highlight') {
+      this.ctx.lineWidth = '5';
+      this.ctx.strokeStyle = '#FEEA4E';
+      this.ctx.rect(info.sx, info.sy, info.width, info.height);
+      this.ctx.stroke();
+      this.drawHightlightBorder();
+      this.drawHightlightArea();
+      this.ctx.clearRect(info.sx, info.sy, info.width, info.height);
+      this.sctx.clearRect(info.sx, info.sy, info.width, info.height);
+    } else if (toolBarType === 'black') {
+      this.drawHightlightBorder();
+      this.drawHightlightArea();
+      this.ctx.fillStyle = 'rgba(0,0,0,.4)';
+      this.ctx.fillRect(info.sx, info.sy, info.width, info.height);
+    }
+  },
+  _documentMouseMove() {
+    this.documentMouseMoveFn = this.documentMouseMoveFn || this.documentMouseMove.bind(this);
+    return this.documentMouseMoveFn;
+  },
+  _elementHelperClick() {
+    this.elementHelperClickFn = this.elementHelperClickFn || this.elementHelperClick.bind(this);
+    return this.elementHelperClickFn;
+  },
+  _windowResize() {
+    this.windowResizeFn = this.windowResizeFn || this.windowResize.bind(this);
+    return this.windowResizeFn;
+  },
+  documentMouseMove(e) {
+    if (this.canvasMD) {
+      if (!this.dragRect) {
+        this.dragRect = true;
+      }
+      let toolBarType = this.state.toolBarType;
+      let clientX = e.clientX + (document.documentElement.scrollLeft + document.body.scrollLeft),
+        clientY = e.clientY + (document.documentElement.scrollTop + document.body.scrollTop),
+        width = this.startX - clientX,
+        height = this.startY - clientY;
+      this.initCanvas();
+      this.drawHightlightBorder();
+      if (toolBarType === 'highlight') {
+        this.ctx.lineWidth = '5';
+        this.ctx.strokeStyle = '#FEEA4E';
+        this.ctx.rect(clientX, clientY, width, height);
+        this.ctx.stroke();
+        this.drawHightlightArea();
+        this.ctx.clearRect(clientX, clientY, width, height);
+        this.sctx.clearRect(clientX, clientY, width, height);
+      } else if (toolBarType === 'black') {
+        this.drawHightlightArea();
+        this.ctx.fillStyle = 'rgba(0,0,0,.4)';
+        this.ctx.fillRect(clientX, clientY, width, height);
+      }
+    } else {
+      this.elementHelper(e);
+    }
+  },
+  elementHelper(e) {
+    let rectInfo = this.inElement(e);
+    let canvas = $('#feedback #feedbackCanvas')[0];
+    if (rectInfo) {
+      canvas.style.cursor = 'pointer';
+      this.drawElementHelper(rectInfo);
+      this.hasHelper = true;
+    } else {
+      if (this.hasHelper) {
+        this.hasHelper = false;
+        this.initCanvas();
+        this.drawHightlightBorder();
+        this.drawHightlightArea();
+      }
+    }
+  },
+  inElement(e) {
+    let x = e.clientX,
+      y = e.clientY;
+    let el = document.elementsFromPoint(x, y)[3];
+    let canvas = $('#feedback #feedbackCanvas')[0];
+    canvas.style.cursor = 'crosshair';
+    if (el && hightLightEl.indexOf(el.nodeName.toLocaleLowerCase()) > -1) {
+      let rect = el.getBoundingClientRect();
+      let rectInfo = {
+        sx: rect.left + (document.documentElement.scrollLeft + document.body.scrollLeft),
+        sy: rect.top + (document.documentElement.scrollTop + document.body.scrollTop),
+        width: rect.width,
+        height: rect.height
+      };
+      return rectInfo;
+    }
+    return false;
+  },
+
+  elementHelperClick(e) {
+    if (this.dragRect) return;
+    let nodeName = e.target.nodeName;
+    if (nodeName !== 'CANVAS') return;
+    let rectInfo = this.inElement(e);
+    if (rectInfo) {
+      let toolBarType = this.state.toolBarType;
+      if (toolBarType === 'highlight') {
+        $.observable(this.state.highlightItem).insert(rectInfo);
+      } else if (toolBarType === 'black') {
+        $.observable(this.state.blackItem).insert(rectInfo);
+      }
+    }
+  },
+  toEditMode() {
+    $.observable(this).setProperty('state.isEditMode', true);
+    $.observable(this).setProperty('screenShotSrc', '');
+    setTimeout(() => {
+      let toolBar = $('#feedback .tool-bar.clearfix')[0],
+        windowWidth = window.innerWidth,
+        windowHeight = window.innerHeight;
+      toolBar.style.left = `${windowWidth * 0.5}px`;
+      toolBar.style.top = `${windowHeight * 0.6}px`;
+    });
+  },
+  clearHightlight(k) {
+    $.observable(this.state.highlightItem).remove(k);
+    setTimeout(() => {
+      this.initCanvas();
+      this.drawHightlightBorder();
+      this.drawHightlightArea();
+    });
+  },
+
+  clearBlack(k, e) {
+    $.observable(this.state.blackItem).remove(k);
+  },
+  editCancel() {
+    $.observable(this).setProperty('state.isEditMode', false);
+    setTimeout(() => {
+      this.shotScreen();
+    });
+  },
+  shotScreen() {
+    if (this.state.loading) return;
+    this.loadingState(true);
+    let highlightItem = this.state.highlightItem;
+    this.switchCanvasVisible(highlightItem.length > 0);
+    Clipper.clip(this._options).then(dataUrl => {
+      $.observable(this).setProperty('screenShotSrc', dataUrl);
+      $('#feedback #screenshotPrev')[0].onload = () => {
+        $.observable(this).setProperty('state.screenshotEdit', true);
+      };
+      this.loadingState(false);
+    }).catch((e) => {
+      $.observable(this).setProperty('state.screenshotEdit', false);
+      this.loadingState(false);
+      console.log(e);
+    });
+  },
+  switchCanvasVisible(visible) {
+    let shadowCanvas = $('#feedback #shadowCanvas')[0];
+    if (visible) {
+      shadowCanvas.removeAttribute('data-html2canvas-ignore');
+    } else {
+      shadowCanvas.setAttribute('data-html2canvas-ignore', 'true');
+    }
+  },
+  setToolBarType(type) {
+    $.observable(this).setProperty('state.toolBarType', type);
+  },
+  loadingState(state) {
+    $.observable(this).setProperty('state.loading', state);
+  },
+  send() {
+    if (this.state.loading) {
+      this.snackbar(this.loadingTip);
+      return;
+    }
+    let text = this.state.text;
+    if (!text) {
+      $.observable(this).setProperty('state.textError', this.requiredTip);
+      $('#feedback textarea').focus();
+      return;
+    }
+    if (typeof this.submitCallback === 'function') {
+      let data = {
+        text: this.state.text
+      };
+      if (this.state.shotOpen) {
+        data.shot = this.screenShotSrc || '';
+      }
+      this.submitCallback(data);
+      this.cancel();
+    }
+  },
+  setTextError() {
+    $.observable(this).setProperty('state.textError', '');
+  },
+  cancel() {
+    this.removeEventListener();
+    $.observable(this).setProperty('state.isOpen', false);
+  },
+  snackbar(msg) {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    $.observable(this).setProperty('state.snackbar', true);
+    $.observable(this).setProperty('state.snackbarMsg', msg || '');
+    this.timer = setTimeout(() => {
+      $.observable(this).setProperty('state.snackbar', false);
+      $.observable(this).setProperty('state.snackbarMsg', '');
+    }, 1500);
+  },
+  checkboxHandle() {
+    $.observable(this).setProperty('state.shotOpen', !this.state.shotOpen);
+    if (!this.state.shotOpen) {
+      this.shotScreen();
+    }
+  },
+  componentDidMount() {
+    if (this.trigger) {
+      this.trigger.addEventListener('click', () => {
+        $.observable(this).setProperty('state.isOpen', true);
+        this.calcHeight();
+        this.addEventListener();
+        if (this.state.shotOpen) {
+          this.shotScreen();
+        }
+      });
+    }
+  }
+};
+export default DEFAULT_OPTION;
